f4a76616b2b30ffb62f3ce673250409edc54b6fd
Kia Choi <sgchoi529@email.arizona.edu>
2018-02-08 20:47:07 -0700

    Completed ex4.c

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
+    printf("Parent process\n");
+    int rc = fork();
 
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child process\n");
+        execl("/bin/ls", "ls", "-l" (char *) NULL);
+    } else {
+        wait(NULL);
+    }
+    
     return 0;
 }
